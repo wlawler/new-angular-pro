23b91f20323d513911b8eb751c39ee01d2edc4e6
wes lawler <wes.lawler2@gmail.com>
2019-08-23 14:18:32 -0400

    finished content children and query list video which is video 5

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,5 +1,5 @@
 import { User } from './auth-form.interface';
-import {Component, Output, EventEmitter, ContentChild,AfterContentInit} from '@angular/core';
+import {Component, Output, EventEmitter, ContentChildren,QueryList, AfterContentInit} from '@angular/core';
 import {AuthRememberComponent} from './auth-remember';
 
 @Component({
@@ -11,16 +11,19 @@ export class AuthFormComponent implements AfterContentInit {
 
   showMessage: boolean;
 
-  @ContentChild(AuthRememberComponent) remember: AuthRememberComponent;
+  @ContentChildren(AuthRememberComponent) remember:QueryList<AuthRememberComponent>;
 
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
   ngAfterContentInit() {
-if(this.remember){
-  this.remember.checked.subscribe((checked: boolean) =>{
-    this.showMessage = checked;
-  });
-};
+ if (this.remember){
+   this.remember.forEach((item) => {
+  item.checked.subscribe((checked: boolean) => this.showMessage = checked);
+});
+ 
+  //this.remember.checked.subscribe((checked: boolean) =>{ this.showMessage = checked;  });
+
+   }
   }
 
   onSubmit(value: User) {
