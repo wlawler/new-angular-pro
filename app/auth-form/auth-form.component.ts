e2ad08c505ad55b49335206357fd6fa7dfaf8340
wes lawler <wes.lawler2@gmail.com>
2019-08-23 14:35:08 -0400

    finished aftercontentinit video which is video 6

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,21 +1,31 @@
 import { User } from './auth-form.interface';
-import {Component, Output, EventEmitter, ContentChildren,QueryList, AfterContentInit} from '@angular/core';
+import {Component, Output, EventEmitter,ViewChild,AfterViewInit, ContentChildren,QueryList, AfterContentInit} from '@angular/core';
 import {AuthRememberComponent} from './auth-remember';
-
+import {AuthMessageComponent } from "./auth-message";
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 @Component({
   selector: 'auth-form',
   templateUrl: "./auth-form.html",
   
 })
-export class AuthFormComponent implements AfterContentInit {
+export class AuthFormComponent implements AfterContentInit, AfterViewInit {
 
   showMessage: boolean;
+
+  @ViewChild(AuthMessageComponent) message:AuthMessageComponent;
 
   @ContentChildren(AuthRememberComponent) remember:QueryList<AuthRememberComponent>;
 
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
+  ngAfterViewInit(){
+
+  }
+
   ngAfterContentInit() {
+    if(this.message) {
+       this.message.days =30;
+    };
  if (this.remember){
    this.remember.forEach((item) => {
   item.checked.subscribe((checked: boolean) => this.showMessage = checked);
