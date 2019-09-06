eed54102546d0df909ec871678d1731a7245f9da
wes lawler <wes.lawler2@gmail.com>
2019-09-06 16:22:38 -0400

    finished video 26

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,4 +12,24 @@ import { Component} from '@angular/core';
 
 
 export class AppComponent  {
+
+  items = [{
+    name: 'Mark Hoppus',
+    age: 44,
+    location: 'California'
+  },{
+    name: 'Tom Delonge',
+    age: 41,
+    location: 'California'
+  },{
+    name: 'Travis Barker',
+    age: 41,
+    location: 'California'
+  }];
+  constructor() {
+    setTimeout(() => {
+      this.items = [...this.items, { name: 'Matt Skiba', age: 40, location: 'California' }];
+    }, 2000);
+  }
 }
+
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CreditCardDirective } from './credit-card/credit-card.directive';
-import { TooltipDirective } from './tooltip/tooltip.directive';
+import { MyForDirective} from './my-for/myfor.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreditCardDirective,
-    TooltipDirective
+    MyForDirective
   ],
   imports: [
     BrowserModule
