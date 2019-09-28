2eba4dc0f64b04a3fca8ec1ce9b5ecbcfbf287d5
wes lawler <wes.lawler2@gmail.com>
2019-09-28 15:16:55 -0400

    finished video 36 FormBuilder API

--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormArray } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { Product } from '../../models/products.interface';
 
 
@@ -46,22 +46,26 @@ export class StockInventoryComponent {
     { "id": 5, "price": 2805, "name": "your dad" },
 
   ];
-  form = new FormGroup({
-    store: new FormGroup({
-      branch: new FormControl(''),
-      code: new FormControl('')
+  form = this.fb.group({
+    store: this.fb.group({
+      branch: '',
+      code: ''
     }),
     selector: this.createStock({}),
 
-    stock: new FormArray([
+    stock: this.fb.array([
       this.createStock({ product_id: 1, quantity: 10 }),
       this.createStock({ product_id: 3, quantity: 150 }),
     ])
   })
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
   createStock(stock) {
-    return new FormGroup({
-      product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
-      quantity: new FormControl(stock.quantity || 10)
+    return this.fb.group({
+      product_id: parseInt(stock.product_id, 10) || '',
+      quantity: stock.quantity || 10
     });
   }
   addStock(stock) {
