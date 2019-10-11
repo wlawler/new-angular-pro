1f921421739a88a5a6ac13dd6f2a0f21f76630cc
wes lawler <wes.lawler2@gmail.com>
2019-10-11 16:27:34 -0400

    finished video 39

--- app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -46,6 +46,10 @@ export class StockSelectorComponent {
      
      onAdd(){
        this.added.emit(this.parent.get('selector').value);
+       this.parent.get('selector').reset({
+         product_id: '',
+         quantity: 10
+       });
 
      }
 }
