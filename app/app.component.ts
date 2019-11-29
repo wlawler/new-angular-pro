85b77f0210d695d9a0477e41ecf439ec48ab5017
wes lawler <wes.lawler2@gmail.com>
2019-11-28 20:52:32 -0500

    finish video 48

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,11 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   constructor(private router: Router) {}
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.events
+    .filter(event => event instanceof NavigationEnd)
+    .subscribe(event => {
+     console.log(event);
+    });
+  }
 }
