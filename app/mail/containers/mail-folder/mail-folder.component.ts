a296bb15a823b6a6efc7268f7abf4b6710f8b252
wes lawler <wes.lawler2@gmail.com>
2019-12-03 08:00:02 -0500

    fixing video 50

--- app/mail/containers/mail-folder/mail-folder.component.ts
+++ app/mail/containers/mail-folder/mail-folder.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
 
 import { Mail } from '../../models/mail.interface';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'mail-folder',
   styleUrls: ['mail-folder.component.scss'],
   template: `
     <h2>Inbox</h2>
-    <mail-item
-      *ngFor="let message of messages"
-      [message]="message">
-    </mail-item>
+  
+   <mail-item
+      *ngFor="let message of (data | async).messages"
+      [message]="message"> 
+    </mail-item> 
   `
 })
 export class MailFolderComponent {
-  messages: Mail[] = [{
-    "id": 1,
-    "folder": "inbox",
-    "from": "Jane Smith",
-    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis, neque at ultricies fringilla, ligula metus",
-    "timestamp": 1487848162905
-  }];
+  data: Observable<{messages: Mail[] }> = this.route.data;
+ constructor(private route: ActivatedRoute ) {}
 }
--- app/mail/mail.module.ts
+++ app/mail/mail.module.ts
@@ -5,20 +5,34 @@ import { RouterModule, Routes } from '@angular/router';
 import { MailFolderComponent } from './containers/mail-folder/mail-folder.component';
 import { MailItemComponent } from './components/mail-item/mail-item.component';
 import { MailAppComponent } from './components/mail-app/mail-app.component';
+import {MailFolderResolve} from './containers/mail-folder/mail-folder.resolve';
+import { MailService} from  './mail.service';
 
 export const ROUTES: Routes = [
-  { path: 'folder/:name', component: MailFolderComponent }
+  { 
+    path: 'folder/:name',
+     component: MailFolderComponent,
+    resolve: {
+         messages: MailFolderResolve
+    }
+    }
+
 ];
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    
   ],
   declarations: [
     MailFolderComponent,
     MailAppComponent,
     MailItemComponent
+  ],
+  providers: [
+    MailService,
+    MailFolderResolve
   ],
   exports: [
     MailAppComponent
