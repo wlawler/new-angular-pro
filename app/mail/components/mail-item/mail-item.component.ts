2d01500e7dd31a7cf917fbc4bf5473667047aa24
wes lawler <wes.lawler2@gmail.com>
2020-01-24 19:22:07 -0500

    finished video 53

--- app/mail/components/mail-item/mail-item.component.ts
+++ app/mail/components/mail-item/mail-item.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 
 import { Mail } from '../../models/mail.interface';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'mail-item',
   styleUrls: ['mail-item.component.scss'],
   template: `
     <a class="mail-item"
-    [routerLink]="['',{outlets: { pane: ['message', message.id]} }]"
-    routerLinkActive="active" >
+    (click)="navigateToMessage()"
+  
+ >
       <h3>
         {{ message.from }}
         <span>{{ message.timestamp | date:'shortTime' }}</span>
@@ -20,4 +21,9 @@ import { Mail } from '../../models/mail.interface';
 export class MailItemComponent {
   @Input()
   message: Mail;
+  constructor(private router: Router) {}
+  navigateToMessage(){
+this.router.navigate(['',{outlets: { pane: ['message', this.message.id]} }]
+)
+  }
 }
